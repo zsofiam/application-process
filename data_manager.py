16cdf87d747eda0b3a57703488512340dcb08673
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-25 14:23:30 +0100

    implement Mentors list and Searching for applicant by name

--- data_manager.py
+++ data_manager.py
@@ -20,7 +20,39 @@ def get_mentors(cursor: RealDictCursor) -> list:
 def get_mentors_by_last_name(cursor: RealDictCursor, last_name: str) -> list:
     query = """
         SELECT first_name, last_name, city
-        FROM mentor
-        ORDER BY first_name"""
+        FROM mentor WHERE last_name = '{0!s}'
+        ORDER BY first_name""".format(last_name)
+    cursor.execute(query)
+
+    # cursor.execute(sql.SQL("select {col1},{col2},{col3} from {table}"
+    #                        " where {col2} = '{0!s}' order by {col1}"
+    #                .format(col1=sql.Identifier('first_name'),
+    #                        col2=sql.Identifier('last_name'),
+    #                        col3=sql.Identifier('city'),
+    #                        table=sql.Identifier('mentor')).format(last_name)))
+
+    # cursor.execute(
+    #     sql.SQL("select {col} from {table} ").
+    #         format(col=sql.Identifier('mentor'),
+    #                table=sql.Identifier('application_process'))
+    # )
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_mentors_by_city(cursor: RealDictCursor, city: str) -> list:
+    query = """
+        SELECT first_name, last_name, city
+        FROM mentor WHERE city = '{0!s}'""".format(city)
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_by_name(cursor: RealDictCursor, applicant_name:str) -> list:
+    query = """
+        SELECT first_name, last_name, phone_number,
+        concat(first_name,' ',last_name) as full_name 
+        FROM applicant WHERE first_name = '{0!s}' or last_name = '{0!s}'""".format(applicant_name)
     cursor.execute(query)
     return cursor.fetchall()
--- main.py
+++ main.py
@@ -13,15 +13,29 @@ def index():
 @app.route('/mentors')
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
+    mentor_city = request.args.get('mentor-city')
 
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
+    elif mentor_city:
+        mentor_details = data_manager.get_mentors_by_city(mentor_city)
     else:
         mentor_details = data_manager.get_mentors()
 
     # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
 
     return render_template('mentors.html', mentors=mentor_details)
+
+
+@app.route("/applicants-phone")
+def get_applicant_data_by_name():
+    if request.args["applicant-name"]:
+        applicant_name = request.args["applicant-name"]
+        print(applicant_name)
+        applicants_details = data_manager.get_applicant_by_name(applicant_name)
+        print(applicants_details)
+        return render_template('applicants_phone.html', applicants=applicants_details)
+    return redirect("/")
 
 
 if __name__ == '__main__':
